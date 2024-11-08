debb2d981fbe75265d134cee27af0de818a275db
Nitin <kumarnitin5814@gmail.com>
2024-11-08 21:53:41 +0530

    Initial Commit

---
+++ HabitBuddy/HabitBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitBuddyApp.swift
+//  HabitBuddy
+//
+//  Created by Nitin ‘s on 08/11/24.
+//
+
+import SwiftUI
+
+@main
+struct HabitBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
